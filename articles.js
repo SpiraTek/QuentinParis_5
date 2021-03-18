faccf53fe08b0f2c2922e0a9ba1b336df19c5c71
Quentin Paris <spiratek31@gmail.com>
2021-03-19 00:34:55 +0100

    Récupération de l'API et intégration des articles. Début CSS

--- articles.js
+++ articles.js
@@ -1,7 +1,7 @@
 // Appel API des produits 
 
-fetch("http://localhost:3000/api/teddies") // first step
-  .then(response => response.json()) // second step
+fetch("http://localhost:3000/api/teddies") 
+  .then(response => response.json()) 
   .then(data => {
     console.log(data)
   })
