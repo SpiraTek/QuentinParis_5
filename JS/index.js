966a23bff0e8034ac975a5e9f93ad2d67ee6974a
Quentin Paris <spiratek31@gmail.com>
2021-03-26 01:16:51 +0100

    Création des pages cart et checkout / Réorganisation du code JS de products et index

--- JS/index.js
+++ JS/index.js
@@ -11,19 +11,13 @@ function getItems () {
          const products = document.getElementById('products') // Div de référence dans le HTML
          for (let index = 0; index < data.length; index++) { // Index des données du array
 
-         // Constantes pour les informations des produits et leurs conteneurs HTML 
+         // Div conteneur
          const container = document.createElement('article')
-         const img = document.createElement('img')
-         const name = document.createElement('h3')
-         const desc = document.createElement('p')
-         const price = document.createElement('h4')
-         const link = document.createElement ('a')
-         const more = document.createElement('button')
-
-         // Paramétrage des données à inclure dans le HTML 
          container.id = "teddies__cards";
          container.className= "teddies";
 
+         // Image 
+         const img = document.createElement('img')
          img.innerHTML = data[index].imageUrl
          img.src = data[index].imageUrl
          img.className = "teddies__img"
@@ -32,19 +26,39 @@ function getItems () {
          img.height = 300
          //description IMG ?
 
+         // Nom
+         const name = document.createElement('h3')
          name.innerHTML = data[index].name
          name.className = "teddies__name"
 
+         // Description
+         const desc = document.createElement('p')
          desc.innerHTML = data[index].description
          desc.className = "teddies__desc"
 
+         // Prix
+         const price = document.createElement('h4')
          price.textContent = data[index].price /100 +'€'
          price.className = "teddies__price"
-         // Lien vers la page products à partir de l'ID dans l'index
-         link.href = "products.html?id=" + data[index]._id
 
+         // Boutton et lien
+         const link = document.createElement ('a')
+         const more = document.createElement('button')
+         link.href = "products.html?id=" + data[index]._id
          more.textContent = "Adoptez le"
          more.className = "more__button seemore"
+
+
+
+
+
+
+
+
+
+
+
+
       
          // Intégration au HTML
          products.append(container)
--- JS/products.js
+++ JS/products.js
@@ -2,37 +2,28 @@
 // Appel API des produits 
 function displayItem () {
     let productLocation = new URLSearchParams(window.location.search);
-    let id = productLocation.get('id')
-    fetch("http://localhost:3000/api/teddies/" + id) // Récupération des données de l'API 
+    let productId = productLocation.get('id')
+    fetch("http://localhost:3000/api/teddies/" + productId) // Récupération des données de l'API 
     .then(function(response){
         return response.json();
     })
     .then(
         (datas => {
         console.log(datas)
-        
         let data = datas;
+
         // Selection du parent HTML
         const singleItem = document.getElementById('details')
 
         // Création des éléments HTML 
-        const container = document.createElement('div')
-        const img = document.createElement('img')
-        const name = document.createElement('h1')
-        const desc = document.createElement('p')
-        const price = document.createElement('h2')  
-        // Gestion du choix des couleurs
-        const color = document.createElement ('select')
-        let options = data.colors;
 
-        options.forEach(function(colors,key) {
-        color[key] = new Option(colors,key); //Boucle permettant de créer une option pour chaque itération de "data.colors"
-        });
-         
-        // Contenu du HTML
+        // Div conteneur
+        const container = document.createElement('div')
         container.id = "teddies__cards";
         container.className= "teddies";
 
+        // Image
+        const img = document.createElement('img')
         img.innerHTML = data.imageUrl
         img.src = data.imageUrl
         img.className = "teddies__img"
@@ -41,16 +32,39 @@ function displayItem () {
         img.height = 300
         //description IMG ?
 
+        // Nom
+        const name = document.createElement('h1')
         name.innerHTML = data.name
         name.className = "teddies__name"
 
+        // Description
+        const desc = document.createElement('p')
         desc.innerHTML = data.description
         desc.className = "teddies__desc"
 
+        // Prix
+        const price = document.createElement('h2')
         price.textContent = data.price /100 +'€'
         price.className = "teddies__price"
-
+        
+        // Gestion du choix des couleurs
+        const color = document.createElement ('select')
+        let options = data.colors;
+        //Boucle permettant de créer une option pour chaque itération de "data.colors"
+        options.forEach(function(colors,key) {
+        color[key] = new Option(colors,key); 
+        });
         color.className = "color__choice"
+
+        // Gestion quantité 
+        const howMany = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let quantity = document.createElement ('select')
+        howMany.forEach(function(qty,key) {
+        quantity[key] = new Option(qty,key); 
+        });
+
+        // Bouton d'ajout au panier
+        const add = document.createElement('button')
 
         
         // Intégration au HTML
@@ -60,9 +74,10 @@ function displayItem () {
         container.append(desc)
         container.append(price)
         container.append(color)
-        color.append(options)
+        container.append(quantity)
+        container.append(add)
     
     })
 
     )}
-displayItem()
+displayItem();
