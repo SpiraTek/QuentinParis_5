d26cbb74c1833bb9a379bd5680ccc84b80098a8d
Quentin Paris <spiratek31@gmail.com>
2021-04-07 12:39:23 +0200

    Récupération Local Storage sur la page Cart.html + correctifs mineurs

--- JS/cart.js
+++ JS/cart.js
@@ -1,3 +1,67 @@
 // Variable stockant les clés et les values qui seront ajoutées au local storage
 let cartStorage = JSON.parse(localStorage.getItem("cart"));
-console.log("kikoo" + cartStorage)
+console.log(localStorage.length);
+
+for (let i = 0; i < cartStorage.length; i++) {
+  console.log(localStorage);
+  let getTable = document.getElementById("cart__container");
+
+  let cartImg = document.createElement("img");
+  cartImg.className = "cartimg";
+  cartImg.src = cartStorage[i].image;
+  getTable.append(cartImg);
+
+  let cartArticle = document.createElement("p");
+  cartArticle.innerHTML = cartStorage[i].name;
+  getTable.append(cartArticle);
+
+  let cartPerLineQuantity = document.createElement("p");
+  cartPerLineQuantity.innerHTML = cartStorage[i].quantity;
+  getTable.append(cartPerLineQuantity);
+
+  let cartPrice = document.createElement("p");
+  cartPrice.innerHTML = cartStorage[i].price + "€";
+  getTable.append(cartPrice);
+
+  //Prix total par ligne
+  let cartTotalPrice = document.createElement("p");
+  cartTotalPrice.innerHTML =
+    cartStorage[i].price * cartStorage[i].quantity + "€";
+  getTable.append(cartTotalPrice);
+
+  let removeBtn = document.createElement("button");
+  removeBtn.innerHTML = "Supprimer du panier";
+  removeBtn.className = "rmvbtn";
+  removeBtn.id = "removeButton" + [i];
+  getTable.append(removeBtn);
+}
+
+// Calcul du prix total
+
+let priceContainer = document.getElementById("subTotal");
+cartStorage.forEach((cart) => {
+  console.log(cart.price, cart.quantity);
+  priceContainer.innerHTML = cart.price * cart.quantity;
+});
+
+// CALCUL DU PRIX TOTAL DU PANIER
+
+/*let getTable = document.getElementById("cart__tablebody")
+
+    let cartArticle = document.createElement("th")
+    cartArticle.innerHTML = cartStorage[i].nom;
+    getTable.append(cartArticle);
+
+    let cartImg = document.createElement("td")
+
+    cartImg.innerHTML = cartStorage[i].image;
+    cartImg.src = "cartStorage[i].image";
+    getTable.append(cartImg)
+
+    let cartPerLineQuantity = document.createElement("td")
+    cartPerLineQuantity.innerHTML = cartStorage[i].Quantité
+    getTable.append(cartPerLineQuantity)
+
+    let cartPrice = document.createElement("td")
+    cartPrice.innerHTML = cartStorage[i].Prix;
+    getTable.append(cartPrice)*/
--- JS/products.js
+++ JS/products.js
@@ -1,4 +1,4 @@
-// Récupération dans l'API et ouverture url avec ID produit
+// Récupération dans l'API et ouverture url avec id produit
 let productLocation = new URLSearchParams(window.location.search);
 let productId = productLocation.get("id");
 fetch("http://localhost:3000/api/teddies/" + productId) // Récupération des données de l'API
@@ -19,7 +19,7 @@ fetch("http://localhost:3000/api/teddies/" + productId) // Récupération des do
     container.id = "teddies__cards";
     container.className = "teddies";
 
-    // Image
+    // image
     const img = document.createElement("img");
     img.innerHTML = data.imageUrl;
     img.src = data.imageUrl;
@@ -29,7 +29,7 @@ fetch("http://localhost:3000/api/teddies/" + productId) // Récupération des do
     img.height = 300;
     //description IMG ?
 
-    // Nom
+    // name
     const name = document.createElement("h1");
     name.innerHTML = data.name;
     name.className = "teddies__name";
@@ -39,7 +39,7 @@ fetch("http://localhost:3000/api/teddies/" + productId) // Récupération des do
     desc.innerHTML = data.description;
     desc.className = "teddies__desc";
 
-    // Prix
+    // price
     const price = document.createElement("h2");
     price.textContent = data.price / 100 + "€";
     price.className = "teddies__price";
@@ -81,30 +81,31 @@ fetch("http://localhost:3000/api/teddies/" + productId) // Récupération des do
 
     // Configuration de l'objet récupérant les données choisies par l'utilisateur
     let selectedOptions = {
-      ID: productId,
-      Nom: data.name,
+      id: productId,
+      name: data.name,
       //Couleur: color.selectedIndex A CONFIGURER
-      Image: data.imageUrl,
-      Prix: data.price / 100 + "€",
-      Quantité: 1,
+      image: data.imageUrl,
+      price: data.price / 100,
+      quantity: 1,
     };
 
-    // --------------  Ajout au panier au click sur le bouton -------------- 
+    // --------------  Ajout au panier au click sur le bouton --------------
 
-    // Selection du bouton dans le DOM 
+    // Selection du bouton dans le DOM
     let addBtn = document.querySelector("button");
 
     // Ajout de l'event listener au click sur le bouton et création de la fonction qui en résulte
     addBtn.addEventListener("click", function () {
-
       // Variable stockant les clés et les values qui seront ajoutées au local storage
-      let cartStorage = JSON.parse(localStorage.getItem("cart"))
+      let cartStorage = JSON.parse(localStorage.getItem("cart"));
 
       // Fonction pour ajouter une fenêtre permettant d'aller au panier ou de revenir au menu principal après ajout au panier
       function confirmation() {
         // Si click sur OK redirection vers page "cart.html"
-        if (window.confirm(`L'ourson a bien été ajouté au panier.
-Pour consulter le panier cliquez sur OK, pour revenir à l'accueil cliquez sur ANNULER`)) {
+        if (
+          window.confirm(`L'ourson a bien été ajouté au panier.
+Pour consulter le panier cliquez sur OK, pour revenir à l'accueil cliquez sur ANNULER`)
+        ) {
           window.location.href = "cart.html";
           // Si click sur ANNULER retour à l'accueil (index.html) du site
         } else {
@@ -114,41 +115,86 @@ Pour consulter le panier cliquez sur OK, pour revenir à l'accueil cliquez sur A
 
       // Fonction pour ajouter le produit selectionné au local storage
       const addToLocalStorage = () => {
-
         // Ajoute l'objet avec les valeurs choisies par l'utilisateur dans le tableau
         cartStorage.push(selectedOptions);
-
         // Transforme l'objet au format JSON et l'envoi dans la "key" du produit dans le local storage
         localStorage.setItem("cart", JSON.stringify(cartStorage));
-      }
+      };
+
       // Si produit déja présent incrémenter la quantité
-// Recherche de la présence du produit dans le local storage. Trouver si dans le local storage un produit possède déjà l'ID du produit qu'on ajoute.
-        
-          // Trouver l'index du produit contenant l'ID
-          //avec l'index je récupère le produit dans le tableau cartStorage    (1 index = 1 ID et 1 COULEUR)
-          //j'ajoute 1 à la quantité  
-          // je renvoie dans le local storage
+      // Recherche de la présence du produit dans le local storage. Trouver si dans le local storage un produit possède déjà l'id du produit qu'on ajoute.
+
+      // Trouver l'index du produit contenant l'id
+      //avec l'index je récupère le produit dans le tableau cartStorage    (1 index = 1 id et 1 COULEUR)
+      //j'ajoute 1 à la quantité
+      // je renvoie dans le local storage
 
       // Si il y'a déjà des produits dans le local storage
-   if(cartStorage) { 
-        addToLocalStorage();
-        console.log("l'ourson est déja dans le panier");
-        confirmation();
+      /* Fonction pour l'ajout d'une quantité au niveau d'un objet déjà existant
+      Celle-ci prend en paramètre un objet (ici l'objet déjà existant representé par une ligne de notre talbeau)
+      La quantite etant toujours incrementée de 1 pas besoin d'autre paramètre
+          il faudrait modifier la fonction afin qu'elle prenne encompte d'autres parametres afin de les utiliser ou non,
+          par ex. => function addXXXXXXToObject(formerObject, autreParamettre) {...}
+      */
+
+      function incrementQuantity(formerObject) {
+        let modifiedObject = {
+          id: formerObject.id,
+          name: formerObject.name,
+          //Couleur: color.selectedIndex A CONFIGURER
+          image: formerObject.image,
+          price: formerObject.price,
+          //petit soucis avec le ++ a toi de regarder plus emplement
+          quantity: ++formerObject.quantity, //Ou +1
+        };
+        return modifiedObject;
       }
 
+      //Booléen permettant de determiner si l'objet qui est testé a été trouvé ou non dans le local storage
+      // De base = true et passe a false dans le cas où un objet se trouve déjà dans le panier
+      let notInCart = true;
+
+      //Test pour savoir si le cartstorage existe déjà
+      if (cartStorage) {
+        //pour chaque ligne de notre "tableau" si l'id de l'objet que l'on désire ajouter au panier existe déjà
+        for (let i = 0; i < cartStorage.length; i++) {
+          if (cartStorage[i].id === productId) {
+            //si l'on determine que l'objet existait déjà on fait passer le booléen notInCart a false (utilisé dans le switch plus bas)
+            notInCart = false;
+            /*à l'emplacement de l'objet dans le tableau on remplace les données présentes par les nouvelles données 
+                (ici en appelant une fonction qui prend en paramètre l'objet initial cartStorage)
+                  et retourne un autre objet modifié.
+                */
+            cartStorage[i] = incrementQuantity(cartStorage[i]);
+          }
+        }
+        //Switch permettant la gestion des différents cas rencontrables, facilite l'ajout d'éventuels nouveaux cas.
+
+        // l'article n'est pas dans le panier, ajout de l'article (création d'un nouvel index)
+        switch (notInCart) {
+          case true:
+            addToLocalStorage();
+            console.log("Un nouveau produit a bien été ajouté au panier.");
+            //confirmation();
+            break;
+          // l'article est déja dans le panier, incrémentation de la quantité
+          case false:
+            localStorage.setItem("cart", JSON.stringify(cartStorage));
+            console.log("Le produit a bien été modifié.");
+            break;
+          default:
+            alert(
+              "une erreur a été rencontrée lors de l'ajout du produit au panier"
+            );
+            console.log("Cas par défaut.");
+        }
+      }
       // Si le local storage est vide création d'un tableau dans celui-ci
-      else{
+      else {
         cartStorage = [];
-        addToLocalStorage ();
-        console.log("Le panier a été initialisé")
-        confirmation();
+        addToLocalStorage();
+        console.log("Le panier a été initialisé.");
+        //confirmation();
       }
-
-          
-    })
-  })
-      // Sinon créer une nouvelle entrée dans le local storage
-    
-    
-  
-  
+    });
+  });
